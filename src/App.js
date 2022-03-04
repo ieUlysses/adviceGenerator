b818aed0d9a70807d0192677b245afad31467918
James Conway <71008025+ieUlysses@users.noreply.github.com>
2022-03-04 15:43:37 +0100

    p3: added context + id test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./Styles/Container.css"
 import Card from './Componants/Card/Card';
 
+import { React, useContext } from "react";
+
+import Context from "./Context/Context";
+
 
 
 function App() {
+
+  const context = useContext(Context);
+  const { id, setId, slip } = context;
+
+  const update = () => {
+    setId(document.getElementById("numberInput").value)
+  }
+
+  console.log(slip.advice)
+
+
   return (
     <>
       <div className='container'>
-        <Card />
+
+        {/* Added test below to bugfix and test context */}
+        <input type="number" name="numberInput" id="numberInput" max="20" min="1" step="1" />
+        <button type="button" onClick={update}>sumbit</button>
+
+        <h1>{slip.slip.advice}</h1>
+
+
       </div>
     </>
   );
